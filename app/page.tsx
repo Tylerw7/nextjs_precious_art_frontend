88ccc53b5444dc837a9070af0003cda1679b26cd
Tylerw7 <t4wettach@gmail.com>
2025-12-01 17:05:30 -0500

    polished navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,12 @@
-"use client";
 
-import Catalog from "@/src/components/catalog/Catalog";
-import { Product } from "@/src/Types/product";
-import { Box, Button, Container, Typography } from "@mui/material";
-import axios from "axios";
-import { useEffect, useState } from "react";
 
 
 export default function Home() {
-  const [products, setProducts] = useState<Product[]>([]);
   
-useEffect(() => {
-  const data = async() => {
-    try {
-      const res = await axios.get('https://localhost:5001/api/products') 
-      setProducts(res.data)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  data()
-},[])
-
-const addProduct = () => {
-  setProducts(prevState => [...prevState,
-    {id: prevState.length + 1,
-    name: 'product' + (prevState.length + 1),
-    price: (prevState.length * 100) + 100,
-    quantityInStock: 100,
-    description: 'test',
-    pictureUrl: 'https://picsum.photo/200',
-    type: 'test',
-    brand: 'test'
-  }])
-}
 
   return (
     <>
-    <Container maxWidth='xl' sx={{mt: 14}}>
-      <Box display='flex' justifyContent='center' gap={3} marginY={3}>
-      <Typography variant="h4">Products</Typography>
-      <Button variant="contained" onClick={addProduct}>Add Product</Button>
-      </Box>
-
-      <Catalog products={products} />
-    </Container>
+    <h2 className="text-[100px]">Home Page</h2>
     </>
   );
 }
--- src/Layouts/NavBar.tsx
+++ src/Layouts/NavBar.tsx
@@ -1,5 +1,5 @@
 import { ShoppingCart } from "@mui/icons-material"
-import { AppBar, Badge, IconButton, List, Toolbar, Typography } from "@mui/material"
+import { AppBar, Badge, Box, IconButton, List, Toolbar, Typography } from "@mui/material"
 import Link from "next/link"
 
 
@@ -15,21 +15,50 @@ const NavBar = () => {
 
   return (
     <AppBar position="fixed">
-        <Toolbar>
+        <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '80px'}}>
+          <Box>
             <Typography variant="h6">Precious Wettach Art</Typography>
+          </Box> 
+
+          <Box> 
             <List className="flex flex-row gap-4">
               {midLinks.map(({title, path}) => (
-                <Link href={path} key={path}><h3>{title}</h3></Link>
+                <Link href={path} key={path}>
+                  <Typography 
+                    variant="h6"
+                    sx={{
+                      '&:hover': {
+                        color: 'grey.500'
+                      }
+                    }}
+                    >
+                  {title.toUpperCase()}
+                  </Typography></Link>
               ))}
             </List>
+            </Box>
 
-            <IconButton size="large">
+            <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '15px'}}> 
+            <IconButton size="large" sx={{color: 'inherit'}}>
               <Badge badgeContent="4" color="secondary">
                   <ShoppingCart />
               </Badge>
             </IconButton>
 
-            <Link href={"/"}><h3>Login</h3></Link>
+            <Link href={"/"}>
+            <Typography 
+              variant="h6"
+              sx={{
+                '&:hover': {
+                  color: 'grey.500'
+                }
+              }}
+            >
+              LOGIN
+              </Typography>
+              </Link>
+              </Box> 
+
         </Toolbar>
     </AppBar>
   )
